e560902ab0cff836675f639cec369bb8cebb1c0d
Lungisani.Nhlengethwa <lungisani.nhlengethwa@altron.com>
2021-09-15 09:10:44 +0200

    Put the quote in a 'card'

--- src/App.js
+++ src/App.js
@@ -4,10 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <img className="lungi-image" src='./lungisani.jpg' alt="lungisani"></img>
-      
-      <div className="Quotes">
-        <p>The hand that rocks the cradle, rules the world</p>
+      <div className="card">
+        <p>The hand that rocks the cradle, rules the world.</p>
       </div>
     </div>
   );
