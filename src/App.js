18645d97411dfe01166ea74842d125fb10b07d5b
lungisani <lungisani@webparam.org>
2021-09-17 16:42:28 +0200

    Change is a constant

--- src/App.js
+++ src/App.js
@@ -4,12 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div class="bg"></div>
+      <div class="lightning flashit"></div>
       <div className="card">
         <p>The hand that rocks the cradle, rules the world.</p>
-      </div>
-
-      <div id="footer">
-        Create your emotions. (c) Your heart.
       </div>
     </div>
   );
